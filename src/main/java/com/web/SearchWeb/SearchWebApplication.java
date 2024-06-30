f2e0713e456bca9b9a97cc6ded349425249d8691
jin2304 <search2304@naver.com>
2024-06-30 22:33:41 +0900

    first commit

---
+++ src/main/java/com/web/SearchWeb/SearchWebApplication.java
@@ -0,0 +1,13 @@
+package com.web.SearchWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/SearchWeb/SearchWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.SearchWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
