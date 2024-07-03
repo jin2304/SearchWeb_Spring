9ec60436eb1d4f6301fe75a11afb0d4c0dc8533a
jin2304 <search2304@naver.com>
2024-07-03 22:50:36 +0900

    fix: 회원가입 과정에서 새로고침 시 발생하는 중복 제출 오류를 PRG 패턴 적용하여 해결

--- src/main/java/com/web/SearchWeb/member/controller/MemberController.java
+++ src/main/java/com/web/SearchWeb/member/controller/MemberController.java
@@ -4,6 +4,7 @@ package com.web.SearchWeb.member.controller;
 
 import com.web.SearchWeb.member.dto.MemberDto;
 import com.web.SearchWeb.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -30,7 +32,9 @@ public class MemberController {
      */
     @GetMapping("/join")
     public String join(Model model) {
-        model.addAttribute("memberDto", new MemberDto()); // 모델에 DTO 객체 추가
+        if (!model.containsAttribute("memberDto")) { // RedirectAttributes를 통해 전달된 "memberDto"가 모델에 포함되어 있는지 확인
+            model.addAttribute("memberDto", new MemberDto());
+        }
         return "/member/join";
     }
 
@@ -39,24 +43,30 @@ public class MemberController {
      *  회원가입
      */
     @PostMapping("/joinProc")
-    public String joinProcess(@Valid MemberDto member, BindingResult bindingResult) {
+    public String joinProcess(@Valid MemberDto member, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         /** 유효성 검사 **/
         // 빈 문자열 검사, 사이즈 체크 등 MemberDto에 정의된 유효성 검사 수행
         if (bindingResult.hasErrors()) {
-            return "/member/join"; // 유효성 검사에 실패하면 회원가입 페이지로 다시 이동
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.memberDto", bindingResult);
+            redirectAttributes.addFlashAttribute("memberDto", member);
+            return "redirect:/join"; // (PRG 패턴 적용)유효성 검사에 실패하면 회원가입 페이지로 다시 이동
         }
 
         // 사용자 아이디 중복 확인
         if (memberService.findByUserName(member.getUsername()) != null) {
             bindingResult.rejectValue("username", "error.member", "이미 존재하는 아이디입니다.");
-            return "/member/join";
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.memberDto", bindingResult);
+            redirectAttributes.addFlashAttribute("memberDto", member);
+            return "redirect:/join";
         }
 
         //비밀번호 확인
         if (!memberService.isPasswordMatching(member)) {
             bindingResult.rejectValue("confirmPassword", "error.member", "입력된 비밀번호와 일치하지 않습니다.");
-            return "/member/join";
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.memberDto", bindingResult);
+            redirectAttributes.addFlashAttribute("memberDto", member);
+            return "redirect:/join";
         }
 
         memberService.joinProcess(member);
