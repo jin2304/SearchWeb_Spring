a44ffde98388291a7d4c912eb39217881d546c9c
jin2304 <search2304@naver.com>
2025-06-21 19:57:22 +0900

    refactor(board): "Board 테이블 3정규형 위반" 따른 member 도메인 비즈니스 로직 수정

--- src/main/java/com/web/SearchWeb/member/service/MemberServiceImpl.java
+++ src/main/java/com/web/SearchWeb/member/service/MemberServiceImpl.java
@@ -2,7 +2,6 @@ package com.web.SearchWeb.member.service;
 
 
 import com.web.SearchWeb.board.dao.BoardDao;
-import com.web.SearchWeb.board.domain.Board;
 import com.web.SearchWeb.member.dao.MemberDao;
 import com.web.SearchWeb.member.domain.Member;
 import com.web.SearchWeb.member.dto.MemberDto;
@@ -12,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 
 //서비스 로직, 트랜잭션 처리
@@ -72,20 +70,7 @@ public class MemberServiceImpl implements MemberService{
     @Override
     @Transactional
     public int updateMember(int memberId, MemberUpdateDto memberUpdateDto) {
-
-        // 1. 회원 정보 수정
         int result = memberDao.updateMember(memberId, memberUpdateDto);
-
-        // 2. 회원 정보 수정이 성공했을 경우, 게시글의 회원정보 업데이트
-        if (result == 1) {
-            List<Board> boards = boardDao.selectBoardListByMemberId(memberId);
-            for (Board board : boards) {
-                board.setJob(memberUpdateDto.getJob());
-                board.setMajor(memberUpdateDto.getMajor());
-                boardDao.updateBoardProfile(board.getBoardId(), memberUpdateDto.getJob(), memberUpdateDto.getMajor());
-            }
-        }
-
         return result;
     }
 }
