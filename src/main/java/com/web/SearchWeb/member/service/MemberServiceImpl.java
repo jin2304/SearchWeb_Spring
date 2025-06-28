d2887d695d7e262fec148daed758965437bc7aba
jin2304 <search2304@naver.com>
2025-06-28 12:38:16 +0900

    bug: 회원 정보 수정 시, 해당 유저가 작성한 댓글의 회원 프로필도 수정되도록 반영

--- src/main/java/com/web/SearchWeb/member/service/MemberServiceImpl.java
+++ src/main/java/com/web/SearchWeb/member/service/MemberServiceImpl.java
@@ -2,6 +2,10 @@ package com.web.SearchWeb.member.service;
 
 
 import com.web.SearchWeb.board.dao.BoardDao;
+import com.web.SearchWeb.board.domain.Board;
+import com.web.SearchWeb.comment.dao.CommentDao;
+import com.web.SearchWeb.comment.domain.Comment;
+import com.web.SearchWeb.comment.dto.UpdateUserProfileCommentDto;
 import com.web.SearchWeb.member.dao.MemberDao;
 import com.web.SearchWeb.member.domain.Member;
 import com.web.SearchWeb.member.dto.MemberDto;
@@ -11,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 
 
 //서비스 로직, 트랜잭션 처리
@@ -19,13 +24,16 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberDao memberDao;
     private final BoardDao boardDao;
+    private final CommentDao commentDao;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public MemberServiceImpl(MemberDao memberDao, BCryptPasswordEncoder bCryptPasswordEncoder, BoardDao boardDao) {
+    public MemberServiceImpl(MemberDao memberDao, BCryptPasswordEncoder bCryptPasswordEncoder, BoardDao boardDao, CommentDao commentDao) {
         this.memberDao = memberDao;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.boardDao = boardDao;
+        this.commentDao = commentDao;
+
     }
 
 
@@ -65,12 +73,28 @@ public class MemberServiceImpl implements MemberService{
 
 
     /**
-     *  회원 수정
+     * 회원 수정
      */
     @Override
     @Transactional
     public int updateMember(int memberId, MemberUpdateDto memberUpdateDto) {
         int result = memberDao.updateMember(memberId, memberUpdateDto);
+
+        // 회원 정보 수정이 성공했을 경우, 게시글 댓글의 회원정보 업데이트
+        if (result == 1) {
+            List<Comment> comments = commentDao.selectCommentsByMemberId(memberId);
+
+            for (Comment comment : comments) {
+                UpdateUserProfileCommentDto commentDto = UpdateUserProfileCommentDto.of(
+                        comment.getCommentId(),
+                        memberUpdateDto.getNickname(),
+                        memberUpdateDto.getJob(),
+                        memberUpdateDto.getMajor()
+                );
+
+                commentDao.updateCommentUserProfile(commentDto);
+            }
+        }
         return result;
     }
 }
